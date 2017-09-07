b2c50d6763bd754f6d3b9c7ae274725599777ef0
hyunyoung2 <le32146@gmail.com>
2017-09-07 15:18:02 +0900

    Create Creating_Directory.py

---
+++ Python3/Tutorial/Creating_Directory/Creating_Directory.py
@@ -0,0 +1,39 @@
+# If you start the python setting 
+# help(os-python keyword)
+
+# In order to check the version of python 
+import sys
+print ("python :" , sys.version)
+print () # for newline 
+
+# In order to Create Directory to store material
+import os 
+
+name_dir = ["MNIST_Dataset", "CIFAR_10_Dataset", "CIRFAR_100_Dataset"]
+
+# Get current Directory from "https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory"
+current_dir = os.getcwd()
+print ("The location of current Directory :", current_dir)
+print () # for nweline
+
+# Checking Whether all the Directory exist already
+for i in range(3) :
+    new_dir = current_dir+"/"+name_dir[i]
+    if not os.path.isdir(new_dir) :
+        print ("Currently, (%s) doesn't exist" % new_dir)
+print () # for nweline
+
+# Create new Directory from "https://stackoverflow.com/questions/1274405/how-to-create-new-folder"
+for i in range(3) :
+    new_dir = current_dir+"/"+name_dir[i]
+    print("The location for New Directory :", new_dir)
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+print () # for nweline
+        
+# Finally, Checking whether the directories is made or not
+for i in range(3) :
+    new_dir = current_dir+"/"+name_dir[i]
+    if os.path.isdir(new_dir) :
+        print ("Currently, (%s) exist" % new_dir)    
+print () # for nweline
